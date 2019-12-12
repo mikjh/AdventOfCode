89ea62a363b5bd71e27278856fdab41a57541d4f
Mikael Hammar <mikael.hammar@zenuity.com>
2019-12-12 21:37:20 +0100

    non-complete part 11.

--- day11/part2.jl
+++ day11/part2.jl
@@ -1,20 +1,67 @@
 using Printf
-using Combinatorics
-using Revise
 
 mutable struct Amp
-    list_to_process::Vector{Int}
+    list_to_process::Vector{Int128}
     op_index::Int
     output::Int
     first::Bool
+    relative_offset::Int
+    extra_mem::Dict{Int,Int128}
+end
+
+function get(amp,index)
+    if index <= length(amp.list_to_process)
+        return amp.list_to_process[index]
+    end
+
+    if index in keys(amp.extra_mem)
+        return amp.extra_mem[index]
+    end
+
+    amp.extra_mem[index] = 0
+    return 0
+end
+
+function set(amp,index,value)
+    if index <= length(amp.list_to_process)
+        amp.list_to_process[index] = value
+        return
+    end
+
+    amp.extra_mem[index] = value
 end
 
 function makeAmp(comma_separated_list)::Amp
-    list_to_process = map(x->parse(Int64,x),split(comma_separated_list,','))
-    return Amp(list_to_process[:], 1, -1,true)
+    list_to_process = map(x->parse(Int128,x),split(comma_separated_list,','))
+    return Amp(list_to_process[:], 1, -1,true,1,Dict{Int,Int128}())
 end
 
-function run(amp::Amp,input1,input2)
+function get_arg(amp,ind,arg,arg_nr)
+    el1 = 0
+    if arg == 0
+        el1 = get(amp,ind)
+    elseif arg == 1
+        el1 = get(amp,amp.op_index+arg_nr)
+    elseif arg == 2
+        el1 = get(amp,amp.relative_offset + get(amp,amp.op_index+arg_nr))
+    end
+    return el1
+end
+
+function get_index(amp,arg,ind_nr)
+    ind = 0
+    if arg == 0
+        ind = get(amp,amp.op_index+ind_nr)+1
+    elseif arg == 1
+        println("ERROR HERE")
+    elseif arg == 2
+        ind = amp.relative_offset + get(amp,amp.op_index+ind_nr)
+    end
+
+    return ind
+end
+
+function intcode(amp::Amp,input)
     while true
         op_with_args = amp.list_to_process[amp.op_index]
 
@@ -34,52 +81,56 @@ function run(amp::Amp,input1,input2)
         if length(op_digits) > 2
             args[1:length(op_digits[3:end])] = op_digits[3:end]
         end
+        #println("")
+        #print("op: ")
+        #print(op)
+        #print(" args: ")
+        #print(args)
+        #print(" op_index: ")
+        #print(amp.op_index)
+        #print(" rel off: ")
+        #print(amp.relative_offset)
 
         if op[1] == 3
-            ind1 = amp.list_to_process[amp.op_index+1]+1
-
-            amp.list_to_process[ind1] = amp.first ? input1 : input2
+            ind1 = 0
+            ind1 = get_index(amp, args[1], 1);
+            set(amp, ind1, input)
             amp.first = false
             amp.op_index+=2
             continue
         end
 
         if op[1] == 4
-            ind1 = amp.list_to_process[amp.op_index+1]+1
-            el1 = 0
-            if args[1] == 0
-                el1 = amp.list_to_process[ind1]
-            elseif args[1] == 1
-                el1 = amp.list_to_process[amp.op_index+1]
-            end
+            ind1 = get(amp,amp.op_index+1)+1
+            el1 = get_arg(amp,ind1,args[1],1)
             amp.op_index+=2
             amp.output=el1
             return false
         end
 
-        ind1 = amp.list_to_process[amp.op_index+1]+1
-        el1 = 0
-        if args[1] == 0
-            el1 = amp.list_to_process[ind1]
-        elseif args[1] == 1
-            el1 = amp.list_to_process[amp.op_index+1]
+        ind1 = get(amp,amp.op_index+1)+1
+        el1  = get_arg(amp,ind1,args[1],1)
+        #print(" e1: ")
+        #print(el1)
+
+        if op[1] == 9
+            amp.relative_offset += el1
+            amp.op_index += 2
+            continue
         end
 
-        ind2 = amp.list_to_process[amp.op_index+2]+1
-        el2 = 0
-        if args[2] == 0
-            el2 = amp.list_to_process[ind2]
-        elseif args[2] == 1
-            el2 = amp.list_to_process[amp.op_index+2]
-        end
+        ind2 = get(amp,amp.op_index+2)+1
+        el2  = get_arg(amp,ind2,args[2],2)
+        #print(" e2: ")
+        #print(el2)
 
         if op[1] == 1
-            ind3 = amp.list_to_process[amp.op_index+3]+1
-            amp.list_to_process[ind3] = el1 + el2
+            ind3 = get_index(amp, args[3], 3);
+            set(amp,ind3,el1+el2)
             amp.op_index+=4
         elseif op[1] == 2
-            ind3 = amp.list_to_process[amp.op_index+3]+1
-            amp.list_to_process[ind3] = el1 * el2
+            ind3 = get_index(amp, args[3], 3);
+            set(amp,ind3,el1*el2)
             amp.op_index+=4
         elseif op[1] == 5
             if el1 != 0
@@ -94,64 +145,98 @@ function run(amp::Amp,input1,input2)
                 amp.op_index += 3
             end
         elseif op[1] == 7
-            ind3 = amp.list_to_process[amp.op_index+3]+1
+            ind3 = get_index(amp, args[3], 3);
             if el1 < el2
-                amp.list_to_process[ind3] = 1
+                set(amp,ind3,1)
             else
-                amp.list_to_process[ind3] = 0
+                set(amp,ind3,0)
             end
             amp.op_index+=4
         elseif op[1] == 8
-            ind3 = amp.list_to_process[amp.op_index+3]+1
+            ind3 = get_index(amp, args[3], 3);
             if el1 == el2
-                amp.list_to_process[ind3] = 1
+                set(amp,ind3,1)
             else
-                amp.list_to_process[ind3] = 0
+                set(amp,ind3,0)
             end
             amp.op_index+=4
         end
     end
 end
 
-function amplifiers(comma_separated_list)
-    amplifiers = permutations(collect(5:9))
-
-    max_output = 0
-    max_amplifier = 0
-
-    for amplifier in amplifiers
-        output = 0
-        index = 0
-        amplifier_opindex = Dict{Int,Amp}(i => makeAmp(comma_separated_list) for i in 1:5)
-
-        halted = false
-        while !halted
-            input = amplifier[index+1]
-            halted_amp = run(amplifier_opindex[index+1],input,output)
-            if halted_amp
-                break
-            end
-
-            output = amplifier_opindex[index+1].output
-
-            index += 1
-            index %= 5
-        end
-
-        if output > max_output
-            max_output = output
-            max_amplifier = amplifier
-        end
+function newDirection(direction, output)
+    if output == 1
+        direction += 1
+    elseif output == 0
+        direction -= 1
     end
 
-    return max_output, max_amplifier
+    if direction < 0
+        direction = 3
+    elseif direction > 3
+        direction = 0
+    end
+
+    return direction
 end
 
-using Test
-#@test amplifiers("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5") == tuple(139629729,[9,8,7,6,5])
+function newPosition(position, direction)
 
-#@test amplifiers("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10") == tuple(18216, [9,7,8,5,6])
-#@test amplifiers("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0") == tuple(54321,[0,1,2,3,4])
-#@test amplifiers("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0") == tuple(65210,[1,0,4,3,2])
-#
-println(amplifiers("3,8,1001,8,10,8,105,1,0,0,21,30,39,64,81,102,183,264,345,426,99999,3,9,1001,9,2,9,4,9,99,3,9,1002,9,4,9,4,9,99,3,9,1002,9,5,9,101,2,9,9,102,3,9,9,1001,9,2,9,1002,9,2,9,4,9,99,3,9,1002,9,3,9,1001,9,5,9,1002,9,3,9,4,9,99,3,9,102,4,9,9,1001,9,3,9,102,4,9,9,1001,9,5,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,99"))
+    if direction == 0
+        position += [0,1]
+    elseif direction == 1
+        position += [1,0]
+    elseif direction == 2
+        position += [0,-1]
+    elseif direction == 3
+        position += [-1,0]
+    end
+
+    return position
+end
+
+function simulate(comma_separated_list)
+    current_position = [0,0]
+    direction = 0
+    plates = Dict(current_position => "w")
+
+    int_code = makeAmp(comma_separated_list)
+
+    halted = false
+    while !halted
+        input = current_position in keys(plates) ? (plates[current_position] == "w" ? 1 : 0) : 0
+        halted = intcode(int_code,input)
+        if halted
+            break
+        end
+        plates[current_position]  = int_code.output == 1 ? "w" : "b"
+        halted = intcode(int_code,input)
+        if halted
+            break
+        end
+        direction = newDirection(direction,int_code.output)
+        println("")
+        print("Input: ")
+        print(input)
+        print(" position: ")
+        print(current_position)
+        current_position = newPosition(current_position,direction)
+        print(" new position: ")
+        print(current_position)
+        print(" direction: ")
+        print(direction)
+        print(" output: ")
+        print(int_code.output)
+    end
+
+    return plates
+end
+
+
+plates = simulate("3,8,1005,8,299,1106,0,11,0,0,0,104,1,104,0,3,8,102,-1,8,10,101,1,10,10,4,10,1008,8,0,10,4,10,1002,8,1,29,1,1007,14,10,2,1106,8,10,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,1,8,10,4,10,1002,8,1,58,3,8,1002,8,-1,10,101,1,10,10,4,10,108,0,8,10,4,10,1002,8,1,80,3,8,1002,8,-1,10,1001,10,1,10,4,10,1008,8,0,10,4,10,102,1,8,103,1,5,6,10,3,8,102,-1,8,10,101,1,10,10,4,10,108,1,8,10,4,10,101,0,8,128,1,106,18,10,1,7,20,10,1006,0,72,1006,0,31,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,0,8,10,4,10,1002,8,1,164,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,1,8,10,4,10,102,1,8,186,1,1007,8,10,1006,0,98,3,8,1002,8,-1,10,1001,10,1,10,4,10,1008,8,0,10,4,10,101,0,8,216,2,102,8,10,1,1008,18,10,1,1108,8,10,1006,0,68,3,8,1002,8,-1,10,1001,10,1,10,4,10,1008,8,1,10,4,10,1001,8,0,253,3,8,102,-1,8,10,1001,10,1,10,4,10,108,1,8,10,4,10,1002,8,1,274,1,1105,7,10,101,1,9,9,1007,9,1060,10,1005,10,15,99,109,621,104,0,104,1,21102,936995738520,1,1,21102,316,1,0,1106,0,420,21101,0,936995824276,1,21102,1,327,0,1106,0,420,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,21102,248129784923,1,1,21102,1,374,0,1105,1,420,21102,29015149735,1,1,21101,385,0,0,1106,0,420,3,10,104,0,104,0,3,10,104,0,104,0,21101,983925826304,0,1,21101,0,408,0,1105,1,420,21102,825012036364,1,1,21101,0,419,0,1105,1,420,99,109,2,22101,0,-1,1,21101,0,40,2,21101,0,451,3,21102,441,1,0,1105,1,484,109,-2,2105,1,0,0,1,0,0,1,109,2,3,10,204,-1,1001,446,447,462,4,0,1001,446,1,446,108,4,446,10,1006,10,478,1101,0,0,446,109,-2,2105,1,0,0,109,4,2102,1,-1,483,1207,-3,0,10,1006,10,501,21102,0,1,-3,21201,-3,0,1,22102,1,-2,2,21102,1,1,3,21101,520,0,0,1106,0,525,109,-4,2105,1,0,109,5,1207,-3,1,10,1006,10,548,2207,-4,-2,10,1006,10,548,21201,-4,0,-4,1105,1,616,21201,-4,0,1,21201,-3,-1,2,21202,-2,2,3,21102,1,567,0,1105,1,525,21202,1,1,-4,21102,1,1,-1,2207,-4,-2,10,1006,10,586,21102,0,1,-1,22202,-2,-1,-2,2107,0,-3,10,1006,10,608,21201,-1,0,1,21102,1,608,0,106,0,483,21202,-2,-1,-2,22201,-4,-2,-4,109,-5,2106,0,0")
+
+using Plots
+
+white_marks = [[key[1] key[2]] for (key,value) in plates if value == "w"]
+
+plot(white_marks[1][1],white_marks[1][2])
